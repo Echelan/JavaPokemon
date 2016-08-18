a93af50e1e4ae70d5578e5c10463edca05399df7
Echelan <andres_movilla1103@hotmail.com>
2016-08-18 17:37:39 -0500

    cleaned comments

--- Violet/src/pokemonviolet/Pokemon.java
+++ Violet/src/pokemonviolet/Pokemon.java
@@ -45,57 +45,27 @@ public class Pokemon {
 	  this.pokeEntry = "";
 	  this.speciesName = "";
 	  this.catchRate = 1;
+      int limite = 0;
 	  
       String[] pokeinfo = new String[30];
-      int limite = 0;
       System.out.println("ID: " + id);
-     // pokeinfo[0] = "" + id;
-        File archivo = new File("pokeDB2.txt");
-            
-            //archivoRuta.setText(archivo.getAbsolutePath());
-            //DefaultTableModel model = (DefaultTableModel) tablaDatos.getModel();
+	  
+        File archivo = new File("pokemonlist.txt");
             try {
                 Scanner eyes = new Scanner(archivo);
                 boolean foundPokemon = false;
-              //  int counter=0;
-               //    System.out.println( eyes.findInLine("\\[[\\d*]\\]") );
-              // eyes.
                 while (eyes.hasNextLine() && foundPokemon == false) {
                     String thisline = eyes.nextLine();
-                //    counter++;
-                //    System.out.println("LINE: " + counter);
-                    //  System.out.println(thisline);
-                    if ( thisline.matches("\\[\\d*\\]") ){
-                    //String[] datos = thisline;
-                    /*
-                   System.out.println( thisline.substring(0, thisline.length()) );
-                   System.out.println( thisline.substring(0, thisline.length()-1) );
-                   System.out.println( thisline.substring(0, thisline.length()-2) );
-                   System.out.println( thisline.substring(1, thisline.length()) );
-                    */
-                //   System.out.println( thisline.substring(1, thisline.length()-1) );
-                   if (Integer.parseInt(thisline.substring(1, thisline.length()-1)) == id ){
-                //    thisline.substring
-                      foundPokemon = true;
-             //         System.out.println( "Found it!" );
-              //     System.out.println( thisline.substring(1, thisline.length()-1) );
-                   //   System.out.println(thisline);
-                   }
-                  }
-              //      String cedula = datos[0];
-               //     String nombre = datos[1];
-                //    int saldo = Integer.parseInt(datos[2]);
-                    
-                 //   model.addRow(new Object[]{cedula, nombre, saldo});
+					if ( thisline.matches("\\[\\d*\\]") ){
+						if (Integer.parseInt(thisline.substring(1, thisline.length()-1)) == id ){
+							foundPokemon = true;
+						}
+					}
                 }
                 if (foundPokemon == true){
                        boolean endInfo = false;
-                 //      int limite = 0;
                     while (eyes.hasNextLine() && endInfo == false) {
                         String thisline = eyes.nextLine();
-                    //    counter++;
-                    //    System.out.println("LINE: " + counter);
-                        //  System.out.println(thisline);
                         if ( thisline.matches("\\[\\d*\\]") ){
                             
                             endInfo = true;
@@ -103,83 +73,10 @@ public class Pokemon {
                             pokeinfo[ limite ] = thisline;
                             limite++;
                         }
-                        
-                        //String[] datos = thisline;
-                        /*
-                       System.out.println( thisline.substring(0, thisline.length()) );
-                       System.out.println( thisline.substring(0, thisline.length()-1) );
-                       System.out.println( thisline.substring(0, thisline.length()-2) );
-                       System.out.println( thisline.substring(1, thisline.length()) );
-                        */
-                    //   System.out.println( thisline.substring(1, thisline.length()-1) );
-                    //   if (Integer.parseInt(thisline.substring(1, thisline.length()-1)) == id ){
-                    //    thisline.substring
-                    //      foundPokemon = true;
-                   //       System.out.println( "Found it!" );
-                   //    System.out.println( thisline.substring(1, thisline.length()-1) );
-                       //   System.out.println(thisline);
-                    //   }
-                  //    }
-                  //      String cedula = datos[0];
-                   //     String nombre = datos[1];
-                    //    int saldo = Integer.parseInt(datos[2]);
-
-                     //   model.addRow(new Object[]{cedula, nombre, saldo});
                     }
-                    //System.out.println( "TXT: " + counter );
                 }
-                
-            } catch (FileNotFoundException ex) {
-                // TODO: No sé encontró el archivo
+            }catch (FileNotFoundException ex) {
             }
-            /*
-        try {
-                String content = readFile("pokeDB.txt", StandardCharsets.UTF_8);
-        //	System.out.println(content);
-                String[] parts = content.split("(?<=},)");
-         //       String part1 = parts[0]; // 004
-        //        String part2 = parts[1]; // 034556
-         //   System.out.println(parts[0]);
-        //    System.out.println("--");                    
-         //   System.out.println(parts[1]);
-         content=null;
-         pokeinfo = parts[id];
-         parts=null;
-         
-        } catch (IOException ex) {
-                Logger.getLogger(GameWindow.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      //  System.out.println(pokeinfo);
-        String[] info = pokeinfo.split(",");
-        
-    //      System.out.println(info[0]);
-        info[0] = info[0].substring(1);
-    //      System.out.println(info[0]);
-        info[0] = info[0].substring(1);
-     //     System.out.println(info[0]);
-          
-        info[info.length-1] = info[info.length-1].substring(0,info[info.length-1].length()-1);
-    //    info[info.length] = info[info.length].substring(0,info[info.length-1].length()-1);
-        for (int i = 0; i < info.length; i++) {
-          info[i] = info[i].substring(1,info[i].length()-1);
-        //  System.out.println(info[i]);
-      }
-          //System.out.println(info[0]);
-		this.id = Integer.parseInt(info[0]);
-        this.speciesName = info[1];
-    //    this.types = types;
-        this.catchrate = Integer.parseInt(info[4]);
-    //    this.baseStats = baseStats;
-    //    this.yieldXP = yieldXP;
-    //    this.yieldEV = yieldEV;
-    //    this.evolves = evolves;
-    //    this.evolvesInto = evolvesInto;
-    //    this.gender = gender;
-    //    this.name = name;
-    //    this.experience = experience;
-    //    this.stats = stats;
-        this.level = 1;
-        */
           
             
             
