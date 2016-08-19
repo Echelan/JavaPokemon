28986ba96a7a02c69f88abf6111cfc78ad1a8342
Echelan <andres_movilla1103@hotmail.com>
2016-08-19 16:35:12 -0500

    added gender and evolutions
    removed extra evolutions
    fixed invisible UI bug
    reorganized UI
    removed thread sleep
    changed catching display to be more detailed
    added nickname

--- Violet/src/pokemonviolet/GameWindow.java
+++ Violet/src/pokemonviolet/GameWindow.java
@@ -49,7 +49,6 @@ public class GameWindow extends JFrame implements WindowListener, ActionListener
     public GameWindow(){
 		
         setLayout(null);
-        setVisible(true);
         setSize(600,500);
         setTitle("Pokemon Violet");
         setResizable(false);
@@ -59,7 +58,7 @@ public class GameWindow extends JFrame implements WindowListener, ActionListener
         walkBtn = new JButton();
         walkBtn.setText("Walk");
 		walkBtn.addActionListener(this);
-        walkBtn.setBounds(10, 10, 160, 70);
+        walkBtn.setBounds(350, 190, 160, 70);
         add(walkBtn);
 		
 		throwBtn = new JButton();
@@ -70,22 +69,22 @@ public class GameWindow extends JFrame implements WindowListener, ActionListener
         add(throwBtn);
         
         pokemonNameDisplay = new JTextField();
-        pokemonNameDisplay.setBounds(10,100,160,70);
+        pokemonNameDisplay.setBounds(10,10,160,70);
 		pokemonNameDisplay.setEditable(false);
         add(pokemonNameDisplay);
         
         pokemonHeightDisplay = new JTextField();
-        pokemonHeightDisplay.setBounds(10,190,160,70);
+        pokemonHeightDisplay.setBounds(10,100,160,70);
 		pokemonHeightDisplay.setEditable(false);
         add(pokemonHeightDisplay);
         
         pokemonWeightDisplay = new JTextField();
-        pokemonWeightDisplay.setBounds(10,280,160,70);
+        pokemonWeightDisplay.setBounds(10,190,160,70);
 		pokemonWeightDisplay.setEditable(false);
         add(pokemonWeightDisplay);
         
 		eventsTextDisplay = new JTextArea();
-		eventsTextDisplay.setBounds(360, 10, 230, 160);
+		eventsTextDisplay.setBounds(350, 10, 235, 160);
 		eventsTextDisplay.setEditable(false);
 		add(eventsTextDisplay);
 		
@@ -96,8 +95,8 @@ public class GameWindow extends JFrame implements WindowListener, ActionListener
 		imgContainerLabel.setIcon(pokemonImgDisplay);
 		imgContainerLabel.setBounds(180,10,POKEIMGWIDTH,POKEIMGHEIGHT);
 		add(imgContainerLabel);
-			
 		
+        setVisible(true);
     }
 
     public static void main(String[] args) throws InterruptedException{
@@ -118,6 +117,107 @@ public class GameWindow extends JFrame implements WindowListener, ActionListener
 		
 		
     }
+
+	public void displayEvent(String event) {
+		
+		eventsTextDisplay.setText(event + "\n" + eventsTextDisplay.getText());
+		
+	}
+	
+	public void walkBtnPress() {
+		stepsToSpawn = stepsToSpawn - 1;
+		if (stepsToSpawn == 0){
+			Random rnd = new Random();
+			currentPokemon = new Pokemon(rnd.nextInt(150)+1);
+			
+			pokemonNameDisplay.setText(currentPokemon.getSpeciesName());
+			pokemonHeightDisplay.setText(currentPokemon.getHeight() + " m.");
+			pokemonWeightDisplay.setText(currentPokemon.getWeight() + " kg.");
+			
+			throwBtn.setEnabled(true);
+			
+			int x = (int)(Math.floor((double)(currentPokemon.getId()-1)%10)*POKEIMGWIDTH);
+			int y = (int)(Math.floor((double)(currentPokemon.getId()-1)/10)*POKEIMGHEIGHT);
+			pokemonImgDisplay = new ImageIcon (allPokemonImgMH.getSubimage(x, y, POKEIMGWIDTH, POKEIMGHEIGHT));
+			imgContainerLabel.setIcon(pokemonImgDisplay);
+				
+			displayEvent("A wild " + currentPokemon.getSpeciesName() + " appeared!");
+			
+			stepsToSpawn = calcSteps();
+		}else{
+			cleanPokemonInfo();
+			
+			displayEvent("You walk through the grass.");
+		}
+	}
+	
+	public void cleanPokemonInfo(){
+		currentPokemon = null;
+		pokemonNameDisplay.setText(" ");
+		pokemonHeightDisplay.setText(" ");
+		pokemonWeightDisplay.setText(" ");
+
+		throwBtn.setEnabled(false);
+
+		int x = (int)(Math.floor((double)(151)%10)*POKEIMGWIDTH);
+		int y = (int)(Math.floor((double)(151)/10)*POKEIMGHEIGHT);
+		pokemonImgDisplay = new ImageIcon (allPokemonImgMH.getSubimage(x, y, POKEIMGWIDTH, POKEIMGHEIGHT));
+		imgContainerLabel.setIcon(pokemonImgDisplay);
+	}
+
+	public void throwBtnPress() {
+		throwBtn.setEnabled(false);
+		displayEvent("You throw a Pokeball!");
+		int shakes = 0;
+		boolean caught = currentPokemon.tryCatch();
+		
+		if (caught){
+			caught = currentPokemon.tryCatch();
+			shakes = shakes + 1;
+			
+			if (caught){
+				caught = currentPokemon.tryCatch();
+				shakes = shakes + 1;
+				
+				if (caught){
+					caught = currentPokemon.tryCatch();
+					shakes = shakes + 1;
+					
+					if (caught){
+						displayEvent("Gotcha! " + currentPokemon.getSpeciesName() + " was caught!");
+						cleanPokemonInfo();
+					}
+				}
+			}
+		}
+		
+		if (!caught){
+			throwBtn.setEnabled(true);
+			displayEvent("The Pokemon broke free after " + shakes + " shakes!");
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == walkBtn){
+			walkBtnPress();
+		}else if (e.getSource() == throwBtn){
+			throwBtnPress();
+		}
+	}
+	public int calcSteps(){
+		int steps = 1;
+		int numDados = 2;
+		int numLados = 3;
+		
+		Random rnd = new Random();
+
+		for (int i = 0; i < numDados; i++) {
+		  steps = steps + (rnd.nextInt(numLados)+1);
+		}
+	//	System.out.println(steps);
+		return steps;
+	}
 	
 	@Override
 	public void windowClosing(WindowEvent e) {
@@ -152,119 +252,5 @@ public class GameWindow extends JFrame implements WindowListener, ActionListener
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-	}
-
-	public void displayEvent(String event) {
-		
-		eventsTextDisplay.setText(event + "\n" + eventsTextDisplay.getText());
-		
-	}
-	
-	public void walkBtnPress() {
-		stepsToSpawn = stepsToSpawn - 1;
-		if (stepsToSpawn == 0){
-			Random rnd = new Random();
-			currentPokemon = new Pokemon(rnd.nextInt(150)+1);
-			
-			pokemonNameDisplay.setText(currentPokemon.getSpeciesName());
-			pokemonHeightDisplay.setText(currentPokemon.getHeight() + " m.");
-			pokemonWeightDisplay.setText(currentPokemon.getWeight() + " kg.");
-			
-			throwBtn.setEnabled(true);
-			
-			int x = (int)(Math.floor((double)(currentPokemon.getId()-1)%10)*POKEIMGWIDTH);
-			int y = (int)(Math.floor((double)(currentPokemon.getId()-1)/10)*POKEIMGHEIGHT);
-			pokemonImgDisplay = new ImageIcon (allPokemonImgMH.getSubimage(x, y, POKEIMGWIDTH, POKEIMGHEIGHT));
-			imgContainerLabel.setIcon(pokemonImgDisplay);
-				
-			displayEvent("A wild " + currentPokemon.getSpeciesName() + " appeared!");
-			
-			stepsToSpawn = calcSteps();
-		}else{
-			currentPokemon = null;
-			pokemonNameDisplay.setText(" ");
-			pokemonHeightDisplay.setText(" ");
-			pokemonWeightDisplay.setText(" ");
-			
-			throwBtn.setEnabled(false);
-			
-			int x = (int)(Math.floor((double)(151)%10)*POKEIMGWIDTH);
-			int y = (int)(Math.floor((double)(151)/10)*POKEIMGHEIGHT);
-			pokemonImgDisplay = new ImageIcon (allPokemonImgMH.getSubimage(x, y, POKEIMGWIDTH, POKEIMGHEIGHT));
-			imgContainerLabel.setIcon(pokemonImgDisplay);
-			
-			displayEvent("You walk through the grass.");
-		}
-	}
-
-	public void throwBtnPress() {
-		throwBtn.setEnabled(false);
-		displayEvent("You throw a Pokeball!");
-		
-		boolean caught = currentPokemon.tryCatch();
-		
-		if (caught){
-			displayEvent("*shake*");
-			caught = currentPokemon.tryCatch();
-			try {
-				Thread.sleep(200);
-			} catch (InterruptedException ex) {
-			}
-				
-			if (caught){
-				displayEvent("*shake*");
-				caught = currentPokemon.tryCatch();
-				try {
-					Thread.sleep(200);
-				} catch (InterruptedException ex) {
-				}
-				
-				if (caught){
-					displayEvent("*shake*");
-					caught = currentPokemon.tryCatch();
-					try {
-						Thread.sleep(200);
-					} catch (InterruptedException ex) {
-					}
-				
-					if (caught){
-						try {
-							Thread.sleep(200);
-						} catch (InterruptedException ex) {
-						}
-
-						displayEvent("Gotcha! " + currentPokemon.getSpeciesName() + " was caught!");
-						walkBtnPress();
-					}
-				}
-			}
-		}
-		
-		if (!caught){
-			throwBtn.setEnabled(true);
-			displayEvent("The Pokemon breaks free!");
-		}
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == walkBtn){
-			walkBtnPress();
-		}else if (e.getSource() == throwBtn){
-			throwBtnPress();
-		}
-	}
-	public int calcSteps(){
-		int steps = 1;
-		int numDados = 2;
-		int numLados = 3;
-		
-		Random rnd = new Random();
-
-		for (int i = 0; i < numDados; i++) {
-		  steps = steps + (rnd.nextInt(numLados)+1);
-		}
-	//	System.out.println(steps);
-		return steps;
 	}
 }
--- Violet/src/pokemonviolet/Pokemon.java
+++ Violet/src/pokemonviolet/Pokemon.java
@@ -53,129 +53,190 @@ public class Pokemon {
 	
 	private String height;
 	private String weight;
-	private String genderRate;
+//	private String genderRate;
 	
-	private boolean canEvolve;
-	private String evolvesInto;
-	private String evolveMethod;
-	private int evolveLevel;
+	private int numEvols;
+	private String[] evolvesInto;
+	private String[] evolveMethod;
+	private int[] evolveLevel;
+	private String[] evolveItem;
 	
 	
 	// 'LOCAL'
-//	String gender;
-//	String nickname;
+	private String gender;
+	private String nickname;
 //	int experience;
 //	int[] stats;
 //	int level;
 	
 	
 	public Pokemon(int id) {
-	  int limite = 0;
-      String[] pokeinfo = new String[NUMATTRIB];
+		int limite = 0;
+		String[] pokeinfo = new String[NUMATTRIB];
 	  
         File archivo = new File("pokeDB.txt");
-            try {
-                Scanner eyes = new Scanner(archivo);
-                boolean foundPokemon = false;
-                while (eyes.hasNextLine() && foundPokemon == false) {
-                    String thisline = eyes.nextLine();
+		try {
+			Scanner eyes = new Scanner(archivo);
+			boolean foundPokemon = false;
+			while (eyes.hasNextLine() && foundPokemon == false) {
+				String thisline = eyes.nextLine();
+				if ( thisline.matches("\\[\\d*\\]") ){
+					if (Integer.parseInt(thisline.substring(1, thisline.length()-1)) == id ){
+						foundPokemon = true;
+					}
+				}
+			}
+			if (foundPokemon == true){
+				   boolean endInfo = false;
+				while (eyes.hasNextLine() && endInfo == false) {
+					String thisline = eyes.nextLine();
 					if ( thisline.matches("\\[\\d*\\]") ){
-						if (Integer.parseInt(thisline.substring(1, thisline.length()-1)) == id ){
-							foundPokemon = true;
+
+						endInfo = true;
+					}else{
+						pokeinfo[ limite ] = thisline;
+						limite++;
+					}
+				}
+			}
+		}catch (FileNotFoundException ex) {
+		}
+		
+		this.id = id;
+		
+		for (int i = 0; i < NUMATTRIB; i++){
+			String[] partes = pokeinfo[i].split("=");
+			if (partes[0].compareTo( "Name" )==0){
+				this.speciesName = partes[1];
+				this.nickname = partes[1];
+			}else if (partes[0].compareTo("InternalName")==0){
+				this.internalName = partes[1];
+			 }else if (partes[0].compareTo("Pokedex")==0){
+				this.pokeEntry = partes[1];
+			}else if (partes[0].compareTo("Kind")==0){
+				this.kind = partes[1];
+			}else if (partes[0].compareTo("BaseHP")==0){
+				this.baseHP = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("BaseAttack")==0){
+				this.baseAttack = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("BaseDefense")==0){
+				this.baseDefense = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("BaseSpeed")==0){
+				this.baseSpeed = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("BaseSpAtk")==0){
+				this.baseSpAtk = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("BaseSpDef")==0){
+				this.baseSpDef = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("CatchRate")==0){
+				this.catchRate = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("GrowthRate")==0){
+				this.growthRate = partes[1];
+			}else if (partes[0].compareTo("StepsToHatch")==0){
+				this.hatchSteps = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("Color")==0){
+				this.color = partes[1];
+			}else if (partes[0].compareTo("Habitat")==0){
+				this.habitat = partes[1];
+			}else if (partes[0].compareTo("YieldHP")==0){
+				this.yieldHP = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("YieldAttack")==0){
+				this.yieldAttack = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("YieldDefense")==0){
+				this.yieldDefense = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("YieldSpeed")==0){
+				this.yieldSpeed = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("YieldSpAtk")==0){
+				this.yieldSpAtk = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("YieldSpDef")==0){
+				this.yieldSpDef = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("YieldEXP")==0){
+				this.yieldEXP = Integer.parseInt(partes[1]);
+			}else if (partes[0].compareTo("Weight")==0){
+				this.weight = partes[1];
+			}else if (partes[0].compareTo("Height")==0){
+				this.height = partes[1];
+			}else if (partes[0].compareTo("GenderRate")==0){
+				/*
+					AlwaysMale
+					FemaleOneEighth
+					Female25Percent
+					Female50Percent
+					Female75Percent
+					FemaleSevenEighths
+					AlwaysFemale
+					Genderless
+				*/
+				String genderRate = partes[1];
+				if (genderRate.compareTo("AlwaysMale") == 0){
+					this.gender = "Male";
+				}else if (genderRate.compareTo("AlwaysFemale") == 0){
+					this.gender = "Female";
+				}else if (genderRate.compareTo("Genderless") == 0){
+					this.gender = "Genderless";
+				}else{
+					Random rnd = new Random();
+					double roll = (rnd.nextInt(99)+1)/100;
+					if (genderRate.compareTo("FemaleOneEighth") == 0 && (roll < 1/8)){
+						this.gender = "Female";
+					}else if (genderRate.compareTo("Female25Percent") == 0 && (roll < 1/4)){
+						this.gender = "Female";
+					}else if (genderRate.compareTo("Female50Percent") == 0 && (roll < 1/2)){
+						this.gender = "Female";
+					}else if (genderRate.compareTo("Female75Percent") == 0 && (roll < 3/4)){
+						this.gender = "Female";
+					}else if (genderRate.compareTo("FemaleSevenEighths") == 0 && (roll < 7/8)){
+						this.gender = "Female";
+					}else{
+						this.gender = "Male";
+					}
+				}
+			}else if (partes[0].compareTo("numEvols")==0){
+				this.numEvols = Integer.parseInt(partes[1]);
+				this.evolvesInto = new String[this.numEvols];
+				this.evolveMethod = new String[this.numEvols];
+				this.evolveLevel = new int[this.numEvols];
+				this.evolveItem = new String[this.numEvols];
+			}else if (partes[0].compareTo("Evolutions")==0){
+				if (this.numEvols > 0){
+					String[] allEvolutionInfo = partes[1].split(",");
+					for (int j = 0; j < allEvolutionInfo.length; j++) {
+						switch (j%3) {
+							case 0:
+								this.evolvesInto[(int)Math.floor(j/3)] = allEvolutionInfo[j];
+							break;
+							case 1:
+								this.evolveMethod[(int)Math.floor(j/3)] = allEvolutionInfo[j];
+							break;
+							case 2:
+								try { 
+									this.evolveLevel[(int)Math.floor(j/3)] = Integer.parseInt(allEvolutionInfo[j]); 
+									this.evolveItem[(int)Math.floor(j/3)] = "";
+								} catch(NumberFormatException e) { 
+									this.evolveLevel[(int)Math.floor(j/3)] = 1;
+									this.evolveItem[(int)Math.floor(j/3)] = allEvolutionInfo[j];
+								}
+							break;
 						}
 					}
-                }
-                if (foundPokemon == true){
-                       boolean endInfo = false;
-                    while (eyes.hasNextLine() && endInfo == false) {
-                        String thisline = eyes.nextLine();
-                        if ( thisline.matches("\\[\\d*\\]") ){
-                            
-                            endInfo = true;
-                        }else{
-                            pokeinfo[ limite ] = thisline;
-                            limite++;
-                        }
-                    }
-                }
-            }catch (FileNotFoundException ex) {
-            }
-          
-            
-            
-	this.id = id;
-		
-    for (int i = 0; i < NUMATTRIB; i++){
-    
-	//	System.out.println( pokeinfo[i] );
-
-         
-		String[] partes = pokeinfo[i].split("=");
-		if (partes[0].compareTo( "Name" )==0){
-			this.speciesName = partes[1];
-		}else if (partes[0].compareTo("InternalName")==0){
-			this.internalName = partes[1];
-		 }else if (partes[0].compareTo("Pokedex")==0){
-			this.pokeEntry = partes[1];
-		}else if (partes[0].compareTo("Kind")==0){
-			this.kind = partes[1];
-		}else if (partes[0].compareTo("BaseHP")==0){
-			this.baseHP = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("BaseAttack")==0){
-			this.baseAttack = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("BaseDefense")==0){
-			this.baseDefense = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("BaseSpeed")==0){
-			this.baseSpeed = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("BaseSpAtk")==0){
-			this.baseSpAtk = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("BaseSpDef")==0){
-			this.baseSpDef = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("CatchRate")==0){
-			this.catchRate = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("GrowthRate")==0){
-			this.growthRate = partes[1];
-		}else if (partes[0].compareTo("StepsToHatch")==0){
-			this.hatchSteps = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("Color")==0){
-			this.color = partes[1];
-		}else if (partes[0].compareTo("Habitat")==0){
-			this.habitat = partes[1];
-		}else if (partes[0].compareTo("YieldHP")==0){
-			this.yieldHP = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("YieldAttack")==0){
-			this.yieldAttack = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("YieldDefense")==0){
-			this.yieldDefense = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("YieldSpeed")==0){
-			this.yieldSpeed = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("YieldSpAtk")==0){
-			this.yieldSpAtk = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("YieldSpDef")==0){
-			this.yieldSpDef = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("YieldEXP")==0){
-			this.yieldEXP = Integer.parseInt(partes[1]);
-		}else if (partes[0].compareTo("Weight")==0){
-			this.weight = partes[1];
-		}else if (partes[0].compareTo("Height")==0){
-			this.height = partes[1];
-		}else if (partes[0].compareTo("GenderRate")==0){
-			this.genderRate = partes[1];
-		}else if (partes[0].compareTo("canEvolve")==0){
-			this.canEvolve = Boolean.getBoolean(partes[1]);
-		}else if (partes[0].compareTo("Evolutions")==0){
-		//	this.evolvesInto = partes[1];
-	//	}else if (partes[0].compareTo("evolveMethod")==0){
-		//	this.evolveMethod = partes[1];
-	//	}else if (partes[0].compareTo("evolveLevel")==0){
-		//	this.evolveLevel = Integer.parseInt(partes[1]);
+				}
+			}
 		}
-    }
-	System.out.println(this.id + ": " + this.speciesName);
-  }
+	}
 
 	public String getSpeciesName() {
 		return speciesName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
 	}
 
 	public String getPokeEntry() {
